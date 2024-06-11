d7a94bf9422c38ed5fe831e4bd5a4cf61def24d9
longlufeng <longlufeng@qq.com>
2024-06-11 22:41:14 +0800

    新增项目

---
+++ j-cloud-zuul/src/test/java/cn/jorian/framework/jcloudzuul/JCloudZuulApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.jorian.framework.jcloudzuul;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCloudZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
